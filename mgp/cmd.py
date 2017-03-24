40933ee8c5fa0dc760229dc7149e9040ed8d12ec
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-03-24 12:16:52 -0600

    Add submit and more options

--- mgp/cmd.py
+++ mgp/cmd.py
@@ -96,10 +96,39 @@ def cli():
 @click.argument("targets", nargs=-1, metavar="FILES")
 @click.option("--dryrun", "-n", default=False, is_flag=True)
 @click.option("--printshellcmds", "-p", default=False, is_flag=True)
-@click.option("--cores", "-j", default=1)
+@click.option("--cores", "-j", "nodes", default=1)
 @click.option("--keepgoing", "-k", default=False, is_flag=True)
 @click.option("--verbose", "-v", default=False, is_flag=True)
 @click.option("--use-conda/--skip-conda", default=True)
+@click.option("--lock/--no-lock")
+@click.option("--cluster-config", "-u", default="cluster.yaml")
+@click.option("--rerun-incomplete","--ri", 'force_incomplete', is_flag=True)
+@click.option("--latency-wait","-w")
 def make(**kwargs):
     "generate target files"
     snakemake.snakemake(resource_filename("mgp", "rules/Snakefile"), **kwargs)
+
+@cli.command()
+@click.argument("targets", nargs=-1, metavar="FILES")
+@click.option("--dryrun", "-n", default=False, is_flag=True)
+@click.option("--printshellcmds", "-p", default=False, is_flag=True)
+@click.option("--cores", "-j", "nodes", default=1024)
+@click.option("--keepgoing", "-k", default=False, is_flag=True)
+@click.option("--verbose", "-v", default=False, is_flag=True)
+@click.option("--use-conda/--skip-conda", default=True)
+@click.option("--lock/--no-lock")
+@click.option("--cluster-config", "-u", default="cluster.yaml")
+@click.option("--rerun-incomplete","--ri", 'force_incomplete', is_flag=True)
+@click.option("--latency-wait","-w", default=0)
+def submit(**kwargs):
+    "generate target files"
+    drmaa = " ".join([
+        '-l nodes=1:ppn={threads}',
+        '-j oe',
+        '-M elmar.pruesse@ucdenver.edu',
+        '-l walltime={cluster.walltime}',
+        '-l mem={cluster.mem}',
+        '-q {cluster.queue}'
+    ])
+    print(repr(kwargs))
+    snakemake.snakemake(resource_filename("mgp", "rules/Snakefile"), drmaa=drmaa, **kwargs)
