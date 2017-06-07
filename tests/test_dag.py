c584e696794eb6c8c1d09b792298f32be1b6df69
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-06-07 10:56:01 -0600

    Turn test params into parametrized test fixture

--- tests/test_dag.py
+++ tests/test_dag.py
@@ -3,35 +3,39 @@ import py
 import logging
 import os
 
-targets = [
-    ## fastq.rules
-    # test import
-    'reports/{}_qc.html',
-    # test rule bb_ecco
-    'reports/{}.ecco_qc.html',
-    # test rule trim_bbduk_adapter
-    'reports/{}.trimAQ10_qc.html',
-    # test rule bbmap_rmhuman
-    'reports/{}.xhum_qc.html',
-    # test rule bbmap_dedupe
-    'reports/{}.ddp_qc.html',
-    # test rule phyloFlash and rule phyloFlash_heatmap
-    'reports/{}_heatmap.pdf',
-    ## assembly.rules
-    # FIXME: some of these should fail currently:
-    # individual assembly
-    'reports/{}.by_ID.mhc_qc.html',
-    # grouped assembly
-    'reports/{}.by_SUBJECT.mhc_qc.html',
-    # complete co-assembly
-    'reports/{}.mhc_qc.html',
-    # blast test
-    '{}.by_Subject.mhc.blast/psa.wcfR.csv',
-    #
-    '{}.ecco.trimAQ10.xhum.by_Subject.mhc.blast.otu/psa.wcfR.otu_table.csv'
+from collections import OrderedDict
+class slice2OrderedDict(object):
+    def __getitem__(self, keys):
+        return OrderedDict([(slice.start, slice.stop) for slice in keys])
+odict = slice2OrderedDict()
+
+
+config_dirs = [
+    'ibd'
 ]
 
-@pytest.fixture(params=['ibd'])
+
+# test subject : build target
+targets = odict[
+    ## fastq.rules
+    'import':         'reports/{}_qc.html',
+    'BBMap_ecco':     'reports/{}.ecco_qc.html',
+    'BBDuk_trim':     'reports/{}.trimAQ10_qc.html',
+    'BBDuk_rm_human': 'reports/{}.xhum_qc.html',
+    'BB_dedupe':      'reports/{}.ddp_qc.html',
+    'phyloFlash':     'reports/{}_heatmap.pdf',
+    ## assembly.rules
+    'assemble_separate': 'reports/{}.by_ID.mhc_qc.html',
+    'assemble_grouped':  'reports/{}.by_SUBJECT.mhc_qc.html',
+    'assemble_joined':   'reports/{}.mhc_qc.html',
+    ## blast.rules
+    'blast_gene_find':   '{}.by_Subject.mhc.blast/psa.wcfR.csv',
+    ## otu.rules
+    'otu_table':         '{}.by_Subject.mhc.blast.otu/psa.wcfR.otu_table.csv'
+]
+
+
+@pytest.fixture(params=config_dirs)
 def project_dir(request, tmpdir):
     data_dir = py.path.local(__file__).dirpath('data', request.param)
     data_dir.copy(tmpdir)
@@ -52,18 +56,26 @@ def make_graph(target, rulegraph=False):
     return DiGraph(AGraph(result.output))
     
 
-@pytest.mark.parametrize("target", targets)
-def test_graph_complete(project_dir, target):
+@pytest.fixture(params=list(targets.values()), ids=list(targets.keys()))
+def build_graph(request, project_dir):
+    target = request.param
     with project_dir.as_cwd():
         from ymp.config import icfg
         icfg.init()
         for ds in icfg:
             G = make_graph(target.format(ds))
-            n_start_nodes = len(
-                [1
-                 for node, degree in G.in_degree().items()
-                 if degree == 0])
-            n_runs = len(icfg[ds].runs)
-            print("Testing start-nodes ({}) == runs ({})"
-                  "".format(n_start_nodes, n_runs))
-            assert n_start_nodes == n_runs
+            R = make_graph(target.format(ds), rulegraph=True)
+            yield (icfg[ds], G, R)
+
+
+def test_graph_complete(build_graph):
+    cfg, G, R = build_graph
+    n_start_nodes = len(
+        [1
+         for node, degree in G.in_degree().items()
+         if degree == 0])
+    n_runs = len(cfg.runs)
+
+    print("Testing start-nodes ({}) == runs ({})"
+          "".format(n_start_nodes, n_runs))
+    assert n_start_nodes == n_runs
