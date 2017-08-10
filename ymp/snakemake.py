85fcf9db8e2bc13fa35fa54bd8768b52294bc589
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-08-10 15:21:07 -0600

    Add comments, cleanup formatting

--- ymp/snakemake.py
+++ ymp/snakemake.py
@@ -12,8 +12,15 @@ from ymp.string import ProductFormatter
 
 
 class ExpandableWorkflow(Workflow):
+    """Adds hook for additional wildcard expansion methods to Snakemake"""
     @staticmethod
     def activate():
+        """Installs the ExpandableWorkflow
+
+        Replaces the Workflow object in the snakemake.workflow module
+        with an instance of this class and initializes default expanders
+        (the snakemake syntax).
+        """
         try:
             from snakemake.workflow import workflow
 
@@ -30,7 +37,7 @@ class ExpandableWorkflow(Workflow):
             pass
 
     @staticmethod
-    def register_expandfuncs(expand_input = None, expand_output = None):
+    def register_expandfuncs(expand_input=None, expand_output=None):
         ExpandableWorkflow.activate()
         try:
             from snakemake.workflow import workflow
@@ -43,6 +50,9 @@ class ExpandableWorkflow(Workflow):
             pass
 
     def input(self, *paths, **kwpaths):
+        """Intercepts arguments passed to "rule: input:" and passes them
+        through registered expander functions.
+        """
         for func in reversed(self.expand_funcs['input']):
             try:
                 (paths, kwpaths) = func(paths, kwpaths)
@@ -52,6 +62,9 @@ class ExpandableWorkflow(Workflow):
         return super().input(*paths, **kwpaths)
 
     def output(self, *paths, **kwpaths):
+        """Intercepts arguments passed to "rule: output:" and passes them
+        through registered expander functions.
+        """
         for func in reversed(self.expand_funcs['output']):
             try:
                 (paths, kwpaths) = func(paths, kwpaths)
@@ -64,13 +77,13 @@ class ExpandableWorkflow(Workflow):
 class BaseExpander(object):
     def __init__(self):
         ExpandableWorkflow.register_expandfuncs(
-            expand_input = self.expand_input,
-            expand_output = self.expand_output
+            expand_input=self.expand_input,
+            expand_output=self.expand_output
         )
-        
-    def format(item, *args, **kwargs):
+
+    def format(self, item, *args, **kwargs):
         return item
-    
+
     def expand(self, item, fields, rec=-1):
         if isinstance(item, str):
             updated = self.format(item, **fields)
@@ -80,8 +93,10 @@ class BaseExpander(object):
             item = updated
         elif hasattr(item, '__call__'):  # function
             _item = item
+
             def late_expand(*args, **kwargs):
-                return self.expand(_item(*args, **kwargs), {'wc':args[0]}, rec=rec+1)
+                return self.expand(_item(*args, **kwargs),
+                                   {'wc': args[0]}, rec=rec+1)
             item = late_expand
         elif isinstance(item, int):
             pass
@@ -92,7 +107,8 @@ class BaseExpander(object):
             for i, subitem in enumerate(item):
                 item[i] = self.expand(subitem, fields, rec=rec+1)
         elif isinstance(item, tuple):
-            return (self.expand(subitem, fields, recurse) for subitem in item)
+            return (self.expand(subitem, fields, rec=rec+1)
+                    for subitem in item)
         else:
             raise ValueError("unable to expand item '{}'".format(repr(item)))
 
@@ -100,7 +116,7 @@ class BaseExpander(object):
 
     def expand_input(self, paths, kwpaths):
         def make_l(path):
-            return lambda wc: self.expand(path, {'wc':wc})
+            return lambda wc: self.expand(path, {'wc': wc})
 
         new_paths = []
         for path in paths:
@@ -108,20 +124,19 @@ class BaseExpander(object):
                 new_paths.append(self.expand(path, {}))
             except KeyError:
                 new_paths.append(make_l(path))
-                
+
         new_kwpaths = {}
         for key, path in kwpaths.items():
             try:
                 new_kwpaths[key] = self.expand(path, {})
             except KeyError:
                 new_kwpaths[key] = make_l(path)
-                
 
         return new_paths, new_kwpaths
 
     def expand_output(self, paths, kwpaths):
-        paths = [self.expand(path,{}) for path in paths]
-        kwpaths = {key:self.expand(path,{}) for key, path in kwpaths.items() }
+        paths = [self.expand(path, {}) for path in paths]
+        kwpaths = {key: self.expand(path, {}) for key, path in kwpaths.items()}
         return paths, kwpaths
 
 
