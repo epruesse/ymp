95e882ee837070cb1297da80ae699006628ffceb
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-08-10 14:36:00 -0600

    Add optional logging to project_dir conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,22 @@
-import pytest
+import logging
+
 import py
+
+import pytest
+
+log = logging.getLogger(__name__)
 
 config_dirs = [
     'ibd'
 ]
 
+
 @pytest.fixture(params=config_dirs)
 def project_dir(request, tmpdir):
     data_dir = py.path.local(__file__).dirpath('data', request.param)
     data_dir.copy(tmpdir)
+    log.info("Created project directory {}".format(tmpdir))
     yield tmpdir
+    # for entry in tmpdir.visit():
+    #    log.info(entry)
     tmpdir.remove()
