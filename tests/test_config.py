ff8ce11b3dfc6ab8d43ae0fb2f0be67188ab00da
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-09-21 14:26:55 -0600

    Test config init on all test datasets

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,9 @@
 import pytest
 
 
-@pytest.mark.parametrize("project_dir", ['ibd', 'complex_data'], indirect=True)
+@pytest.mark.parametrize("project_dir",
+                         ['ibd', 'toy', 'mpic', 'complex_data'],
+                         indirect=True)
 def test_config(project_dir):
     with project_dir.as_cwd():
         from ymp.config import icfg
