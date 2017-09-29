c35bc5e778242dd1d16c24ab50af84508e43b130
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2017-09-29 11:16:47 -0600

    Change theme to boostrap [ci skip]

--- doc/conf.py
+++ doc/conf.py
@@ -99,7 +99,8 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+#html_theme = "sphinx_rtd_theme"
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
