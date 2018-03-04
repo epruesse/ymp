5fc5ee341ab1788140a2b2b7eb40b04db29e7ccd
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2018-03-04 16:43:30 -0700

    Improve handling of reference archives

--- ymp/references.py
+++ ymp/references.py
@@ -1,25 +1,71 @@
 import logging
-from typing import Optional
+from typing import Optional, Dict
 import os
+from hashlib import sha1
 
 from ymp.util import make_local_path
+from ymp.snakemake import make_rule
 
 log = logging.getLogger(__name__)
+
+
+class Archive(object):
+    name = None
+    hash = None
+    tar = None
+    dirname = None
+    strip_components = None
+    files = None
+
+    def __init__(self, name, dirname, tar, strip, files):
+        self.name = name
+        self.dirname = name
+        self.tar = tar
+        self.strip = strip
+        self.files = files
+
+        self.hash = sha1(self.tar.encode('utf-8')).hexdigest()[:8]
+
+    def get_files(self):
+        return {fn: os.path.join(self.dirname,
+                                 "_unpacked_" + self.hash,
+                                 fn)
+                for fn in self.files}
+
+    def get_params(self):
+        return {
+        }
+
+    def make_unpack_rule(self, baserule: 'Rule'):
+        return make_rule(
+            name="unpack_{}_{}".format(self.name, self.hash),
+            lineno=0,
+            snakefile=__name__,
+            parent=baserule,
+            input=([], {'tar': self.tar}),
+            output=([], {'files': list(self.get_files().values())}),
+            params=([], {'strip': self.strip,
+                         'dirname': self.dirname})
+        )
 
 
 class Reference(object):
     """
     Represents (remote) reference file/database configuration
     """
+
     def __init__(self, cfgmgr, reference, cfg):
         self.name = reference
         self.cfgmgr = cfgmgr
         self.cfg = cfg
         self.files = {}
         self.archives = []
+        self.dir = os.path.join(self.cfgmgr.dir.references,
+                                self.name)
+
         for rsc in cfg:
             if isinstance(rsc, str):
-                log.error(self.name)
+                rsc = {'url': rsc}
             downloaded_path = make_local_path(self.cfgmgr, rsc['url'])
             type_name = rsc['type'].lower() if 'type' in rsc else 'fasta'
             if type_name == 'fasta':
@@ -27,28 +73,16 @@ class Reference(object):
             elif type_name == 'fastp':
                 self.files['ALL.contigs.fastp.gz'] = downloaded_path
             elif type_name == 'dir':
-                files = [
-                    os.path.join(
-                        self.cfgmgr.dir.references,
-                        self.name,
-                        "_unpacked_{}".format(len(self.archives)),
-                        fn)
-                    for fn in rsc['files']
-                ]
-                for fn in rsc['files']:
-                    self.files[fn] = files
-                strip_components = rsc.get('strip_components', 0)
-                self.archives.append((downloaded_path, strip_components))
+                archive = Archive(name=self.name,
+                                  dirname=self.dir,
+                                  tar=downloaded_path,
+                                  files=rsc['files'],
+                                  strip=rsc.get('strip_components', 0))
+                self.files.update(archive.get_files())
+                self.archives.append(archive)
             else:
                 log.debug("unknown type {} used in reference {}"
                           "".format(type_name, self.name))
-
-    def __str__(self):
-        res = "{refdir}/{refname}/ALL.contigs".format(
-            refdir=self.cfgmgr.dir.references,
-            refname=self.name
-        )
-        return res
 
     def get_file(self, filename):
         log.debug("getting {}".format(filename))
@@ -61,19 +95,19 @@ class Reference(object):
                 "No file {} in Reference {}"
                 "".format(filename, self.name).replace(" ", "_"))
 
-    def get_archive(self, number):
-        return self.archives[int(number)][0]
-
-    def get_strip(self, number):
-        return self.archives[int(number)][1]
-
-    @property
-    def dir(self):
-        return os.path.join(self.cfgmgr.dir.references, self.name)
+    def make_unpack_rules(self, baserule: 'Rule'):
+        for archive in self.archives:
+            yield archive.make_unpack_rule(baserule)
 
 
-def load_references(cfgmgr, cfg: Optional[dict]) -> dict[str, Reference]:
+    # fixme remove?
+    def __str__(self):
+        return os.path.join(self.dir, "ALL.contigs")
+
+
+def load_references(cfgmgr, cfg: Optional[dict]) -> Dict[str, Reference]:
     if not cfg:
         return {}
-    return {name: Reference(cfgmgr, name, data)
-            for name, data in cfg.items()}
+    references = {name: Reference(cfgmgr, name, data)
+                  for name, data in cfg.items()}
+    return references
