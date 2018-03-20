55bafe24c638cf9804cc03dba161ae433da330e9
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2018-03-19 18:43:50 -0600

    Add profiling option to `ymp`

--- ymp/cli/__init__.py
+++ ymp/cli/__init__.py
@@ -20,6 +20,26 @@ def install_completion(ctx, attr, value):
     exit(0)
 
 
+def install_profiler(ctx, attr, value):
+    if not value or ctx.resilient_parsing:
+        return value
+    import yappi
+    import atexit
+
+    def dump_profile():
+        profile = yappi.get_func_stats()
+        profile.sort("ttot")
+        profile.print_all(out=value, columns={
+            0: ("name", 120),
+            1: ("ncall", 10),
+            2: ("tsub", 8),
+            3: ("ttot", 8),
+            4: ("tavg", 8)})
+
+    yappi.start()
+    atexit.register(dump_profile)
+
+
 @group()
 @click.version_option(version=ymp.__version__)
 @click.option(
@@ -27,6 +47,11 @@ def install_completion(ctx, attr, value):
     callback=install_completion, expose_value=False,
     help="Install command completion for the current shell. "
          "Make sure to have psutil installed.")
+@click.option(
+    '--profile', type=click.File(mode="w"),
+    callback=install_profiler, expose_value=False,
+    help="Profile execution time using Yappi"
+)
 def main(**kwargs):
     """
     Welcome to YMP!
