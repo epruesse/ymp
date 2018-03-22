67bb50493f9a20f1f00a5fbc9a5832905313d7ed
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2018-03-22 14:54:15 -0600

    Add __main__ for calling `python -m ymp`

---
+++ ymp/__main__.py
@@ -0,0 +1,12 @@
+"""
+This allows calling the YMP cli via ``python -m``
+
+>>> python -m ymp.cli show references -v
+"""
+
+import sys
+from ymp.cli import main
+
+if __name__ == "__main__":
+    sys.argv[0] = "python -m ymp.cli"
+    main()
