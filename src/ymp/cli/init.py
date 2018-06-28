1f01968f7207e3e7b02d451dab8ea9a22323071c
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2018-06-27 19:07:55 -0600

    Add "ymp init demo" command

--- src/ymp/cli/init.py
+++ src/ymp/cli/init.py
@@ -1,6 +1,8 @@
 "Implements subcommands for ``ymp init``"
 
 import logging
+import os
+import shutil
 import subprocess as sp
 
 import click
@@ -71,3 +73,43 @@ def project(name, yes):
     cfg.projects[name] = {'data': None}
     log.warning("Saving config")
     cfg.save()
+
+
+@init.command()
+def demo():
+    """
+    Copies YMP tutorial data into the current working directory
+    """
+    click.echo("Copying tutorial data to current working directory...")
+    cwd_path = os.getcwd()
+    cwd_files = os.listdir(cwd_path)
+    demo_path = os.path.join(ymp._rsc_dir, "demo")
+    demo_files = os.listdir(demo_path)
+    conflicts = [f for f in demo_files if f in cwd_files]
+    if len(cwd_files) > 10:
+        click.confirm(
+            "WARNING: "
+            "The current working directory already contains a lot of files.\n"
+            "  Using an empty directory to start with is highly suggested.\n"
+            "  Do you want to continue?", abort=True)
+    if conflicts:
+        click.confirm(
+            "WARNING: "
+            "This operation would overwrite the following files: {}\n"
+            "  Do you want to continue?".format(conflicts), abort=True)
+        for f in conflicts:
+            if os.path.isdir(f):
+                shutil.rmtree(f)
+            else:
+                os.unlink(f)
+    for f in demo_files:
+        src = os.path.join(demo_path, f)
+        dst = os.path.join(cwd_path, f)
+        if os.path.isdir(src):
+            shutil.copytree(src, dst)
+        else:
+            shutil.copy(src, dst)
+    click.echo("done.")
+    click.echo("")
+    click.echo("Try running 'ymp make toy.assemble_megahit.map_bbmap',")
+    click.echo("or see https://ymp.readthedocs.io for more examples.")
