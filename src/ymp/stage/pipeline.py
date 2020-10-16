17d70934afbf2baaf54fdafa226321cef5a8cce3
Elmar Pruesse <elmar.pruesse@ucdenver.edu>
2020-10-15 20:55:00 -0600

    Allow hiding intermediate output in pipelines

--- src/ymp/stage/pipeline.py
+++ src/ymp/stage/pipeline.py
@@ -7,6 +7,7 @@ stages.
 import logging
 import os
 
+from collections import OrderedDict
 from typing import Dict, List, Set
 
 from ymp.stage import StageStack, find_stage
@@ -26,50 +27,69 @@ class Pipeline(ConfigStage):
 
     Example:
         pipelines:
-            my_pipeline:
-              - stage_1
+          my_pipeline:
+            hide: false
+            stages:
+              - stage_1:
+                  hide: true
               - stage_2
               - stage_3
     """
     def __init__(self, name: str, cfg: List[str]) -> None:
         super().__init__(name, cfg)
-        self.stage_names: List[str] = cfg
         self._outputs: Dict[str, str] = None
+
+        #: If true, outputs of stages are hidden by default
+        self.hide_outputs = getattr(cfg, "hide", False)
+        #: Dictionary of stages with configuration options for each
+        self.stages = OrderedDict()
+        for stage in cfg.stages:
+            if isinstance(stage, str):
+                self.stages[stage] = {}
+            else:
+                stage_name = next(iter(stage))
+                self.stages[stage_name] = stage[stage_name]
+
+    @property
+    def stage_names(self):
+        """Names of the stages comprising this pipeline"""
+        return list(self.stages.keys())
+
+    @property
+    def pipeline(self):
+        return "." + ".".join(self.stage_names)
+
+    def _make_outputs(self) -> Dict[str, str]:
+        outputs = {}
+        path = ""
+        for stage_name, cfg in self.stages.items():
+            stage = find_stage(stage_name)
+            path = ".".join((path, stage_name))
+            if not cfg.get("hide", self.hide_outputs):
+                outputs.update(stage.get_outputs(path))
+        return outputs
 
     @property
     def outputs(self) -> Dict[str, str]:
         """The outputs of a pipeline are the sum of the outputs
         of each component stage. Outputs of stages further down
         the pipeline override those generated earlier.
-
-        TODO: Allow hiding the output of intermediary stages.
         """
-        outputs = {}
-        path = ""
-        for stage_name in self.stage_names:
-            stage = find_stage(stage_name)
-            path = ".".join((path, stage_name))
-            stage_outputs = stage.outputs
-            if isinstance(stage_outputs, set):
-                outputs.update({output: path for output in stage_outputs})
-            else:
-                outputs.update(stage_outputs)
-        return outputs
+        if self._outputs is None:
+            self._outputs = self._make_outputs()
+        return self._outputs
 
     def can_provide(self, inputs: Set[str]) -> Dict[str, str]:
         """Determines which of ``inputs`` this stage can provide.
 
         The result dictionary values will point to the "real" output.
         """
-        return {
+        res = {
             output: path
             for output, path in self.outputs.items()
             if output in inputs
         }
-
-    @property
-    def pipeline(self):
-        return "." + ".".join(self.stage_names)
+        return res
 
     def get_path(self, stack):
         prefix = stack.name.rsplit('.',1)[0]
@@ -86,4 +106,3 @@ class Pipeline(ConfigStage):
         realstack = stack.get(self.get_path(stack))
         targets.extend(realstack.stage.get_all_targets(realstack))
         return targets
-
